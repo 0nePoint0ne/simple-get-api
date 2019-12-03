407b0b3b135836056a2d3bbe17f283bc4852b64e
0nePoint0ne <40418659+0nePoint0ne@users.noreply.github.com>
2019-12-04 01:09:17 +0900

    code clean up

--- simple_get_api.js
+++ simple_get_api.js
@@ -7,7 +7,7 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-
+// Items dictinary (Later we will show how to connect a database to your api)
 var items = {
     'product_ids': {0: 'apple', 1: 'mango', 2: 'sugar'},
     'items':
@@ -18,13 +18,11 @@ var items = {
         }
 
 }
-
+// Gives users list of items
 app.get("/items", (req, res, next) => {
     res.json(items['product_ids']);
    });
-
-app.get("/item-data?", (req, res, next) => {
-
-    req.query.product_id
+// Gives users item data back
+app.get("/item-data", (req, res, next) => {
     res.json(items['items'][req.query.product_id]);
    });
